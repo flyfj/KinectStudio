dee0e888b4d26af24f134bf6701285ac786a1050
Jie Feng <flyfengjie@gmail.com>
2013-01-22 01:38:23 -0500

    add a kinect project

---
+++ KinectTest/KinectTest/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+
+
+namespace KinectTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private KinectSensor kinect_sensor;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            // enumerate and fetch an available sensor
+            foreach (var potentialsensor in KinectSensor.KinectSensors)
+            {
+                if (potentialsensor.Status == KinectStatus.Connected)
+                {
+                    kinect_sensor = potentialsensor;
+                    break;
+                }
+            }
+
+            // enable data stream
+            if(kinect_sensor != null)
+            {
+                kinect_sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+                kinect_sensor.DepthStream.Enable(DepthImageFormat.Resolution320x240Fps30);
+                kinect_sensor.SkeletonStream.Enable();
+                kinect_sensor.ColorStream.Enable(ColorImageFormat.InfraredResolution640x480Fps30);
+            }
+
+            MessageBox.Show("Kinect enabled.");
+
+            // start kinect
+            //kinect_sensor.Start();
+
+
+
+        }
+    }
+}
