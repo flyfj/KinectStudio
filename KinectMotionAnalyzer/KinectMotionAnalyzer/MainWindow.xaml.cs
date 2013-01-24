f640ff4c380f758f864259f76398672c54a760b6
Jie Feng <flyfengjie@gmail.com>
2013-01-24 15:22:43 -0500

    add new code

---
+++ KinectMotionAnalyzer/KinectMotionAnalyzer/MainWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+
+
+namespace KinectMotionAnalyzer
+{
+    using KinectMotionAnalyzer.Processors;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private KinectDataStreamManager kinect_data_manager;
+        private KinectSensor kinect_sensor;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            kinect_data_manager = new KinectDataStreamManager();
+            display_image.Source = kinect_data_manager.StreamDataBitmap;
+
+            if (!InitKinect())
+                MessageBox.Show("Kinect not connected.");
+            else
+                MessageBox.Show("Kinect initialized.");
+        }
+
+  
+        private bool InitKinect()
+        {
+            // enumerate and fetch an available sensor
+            foreach (var potentialsensor in KinectSensor.KinectSensors)
+            {
+                if (potentialsensor.Status == KinectStatus.Connected)
+                {
+                    kinect_sensor = potentialsensor;
+                    break;
+                }
+            }
+
+            // enable data stream
+            if (kinect_sensor != null)
+            {
+                kinect_sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+                kinect_sensor.DepthStream.Enable(DepthImageFormat.Resolution320x240Fps30);
+                kinect_sensor.SkeletonStream.Enable();
+                kinect_sensor.ColorStream.Enable(ColorImageFormat.InfraredResolution640x480Fps30);
+            }
+            else
+                return false;
+
+            //MessageBox.Show("Kinect enabled.");
+
+            kinect_sensor.ColorFrameReady += kinect_colorframe_ready;
+            kinect_sensor.DepthFrameReady += kinect_depthframe_ready;
+
+            return true;
+        }
+
+        // data ready handlers
+        void kinect_colorframe_ready(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (var frame = e.OpenColorImageFrame())
+            {
+                if (frame == null)
+                    return;
+
+                kinect_data_manager.UpdateColorData(frame);
+            }
+        }
+
+        void kinect_depthframe_ready(object sender, DepthImageFrameReadyEventArgs e)
+        {
+            using (var frame = e.OpenDepthImageFrame())
+            {
+                if (frame == null)
+                    return;
+
+                kinect_data_manager.UpdateDepthData(frame);
+            }
+        }
+
+        void kinect_skeletonframe_ready(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            using (var frame = e.OpenSkeletonFrame())
+            {
+                if (frame == null)
+                    return;
+
+                kinect_data_manager.UpdateSkeletonData(frame);
+            }
+        }
+
+
+        //
+        private void runBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(runBtn.Content.ToString() == "Start Kinect")
+            {
+                kinect_sensor.Start();
+                runBtn.Content = "Stop Kinect";
+            }
+            else
+            {
+                kinect_sensor.Stop();
+                runBtn.Content = "Start Kinect";
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (kinect_sensor != null)
+            {
+                kinect_sensor.Stop();
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ KinectMotionAnalyzer/KinectMotionAnalyzer/Processors/KinectDataStreamManager.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows;
+using Microsoft.Kinect;
+using System.Linq.Expressions;
+
+
+namespace KinectMotionAnalyzer.Processors
+{
+
+    public abstract class Notifier: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged<T>(Expression<Func<T>> propertyExpression)
+        {
+            if (propertyExpression == null)
+                return;
+ 
+            var body = propertyExpression.Body as MemberExpression;
+            if (body == null)
+                return;
+
+            string propertyname = body.Member.Name;
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+            }
+
+        }
+
+    }
+
+    /// <summary>
+    /// used for fetch data and update UI
+    /// </summary>
+    public class KinectDataStreamManager: Notifier
+    {
+        // data property
+        public WriteableBitmap StreamDataBitmap;
+
+        // intermediate storage for color image pixel data
+        private byte[] colorPixelData;
+
+        // Intermediate storage for the depth data received from the camera
+        private DepthImagePixel[] depthPixels;
+
+        public void UpdateColorData(ColorImageFrame frame)
+        {
+            // update property value
+            if (colorPixelData == null)
+            {
+                colorPixelData = new byte[frame.PixelDataLength];
+            }
+
+            frame.CopyPixelDataTo(colorPixelData);
+
+            if (StreamDataBitmap == null)
+            {
+                StreamDataBitmap = new WriteableBitmap(frame.Width, frame.Height, 96, 96,
+                    PixelFormats.Bgr32, null);
+            }
+
+            int stride = frame.Width * sizeof(int);
+            Int32Rect drawRect = new Int32Rect(0, 0, frame.Width, frame.Height);
+            StreamDataBitmap.WritePixels(drawRect, colorPixelData, stride, 0);
+
+            // notify...
+            RaisePropertyChanged(() => StreamDataBitmap);
+
+        }
+
+        public void UpdateDepthData(DepthImageFrame frame)
+        {
+            if (depthPixels == null)
+            {
+                depthPixels = new DepthImagePixel[frame.PixelDataLength];
+            }
+
+            frame.CopyDepthImagePixelDataTo(depthPixels);
+
+            // Get the min and max reliable depth for the current frame
+            int minDepth = frame.MinDepth;
+            int maxDepth = frame.MaxDepth;
+
+            // Convert the depth to RGB
+            int colorPixelIndex = 0;
+            for (int i = 0; i < this.depthPixels.Length; ++i)
+            {
+                // Get the depth for this pixel
+                short depth = depthPixels[i].Depth;
+
+                // To convert to a byte, we're discarding the most-significant
+                // rather than least-significant bits.
+                // We're preserving detail, although the intensity will "wrap."
+                // Values outside the reliable depth range are mapped to 0 (black).
+
+                // Note: Using conditionals in this loop could degrade performance.
+                // Consider using a lookup table instead when writing production code.
+                // See the KinectDepthViewer class used by the KinectExplorer sample
+                // for a lookup table example.
+                byte intensity = (byte)(depth >= minDepth && depth <= maxDepth ? depth : 0);
+
+                // Write out blue byte
+                colorPixelData[colorPixelIndex++] = intensity;
+
+                // Write out green byte
+                colorPixelData[colorPixelIndex++] = intensity;
+
+                // Write out red byte                        
+                colorPixelData[colorPixelIndex++] = intensity;
+
+                // We're outputting BGR, the last byte in the 32 bits is unused so skip it
+                // If we were outputting BGRA, we would write alpha here.
+                ++colorPixelIndex;
+            }
+
+
+            if (StreamDataBitmap == null)
+            {
+                StreamDataBitmap = new WriteableBitmap(frame.Width, frame.Height, 96, 96,
+                    PixelFormats.Bgr32, null);
+            }
+
+            // write to bitmap
+            int stride = frame.Width * sizeof(int);
+            Int32Rect drawRect = new Int32Rect(0, 0, frame.Width, frame.Height);
+            StreamDataBitmap.WritePixels(drawRect, colorPixelData, stride, 0);
+        }
+
+        public void UpdateSkeletonData(SkeletonFrame frame)
+        {
+
+        }
+
+
+
+    }
+}
