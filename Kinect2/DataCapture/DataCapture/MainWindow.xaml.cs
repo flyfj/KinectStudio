3a2d071908b8fef3e8203bdb91c75606633dba8b
Jie Feng <jiefengdev@gmail.com>
2015-05-14 14:20:53 -0400

    added kinect2 folder

---
+++ Kinect2/DataCapture/DataCapture/MainWindow.xaml.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Microsoft.Kinect;
+using System.IO;
+
+namespace DataCapture
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private KinectSensor kinectSensor = null;
+
+        private ColorFrameReader colorFrameReader = null;
+        private DepthFrameReader depthFrameReader = null;
+
+        private WriteableBitmap colorBitmap = null;
+        private WriteableBitmap depthBitmap = null;
+        private FrameDescription colorFrameDescription = null;
+        private FrameDescription depthFrameDescription = null;
+
+        private const int MapDepthToByte = 8000 / 256;
+        private byte[] depthPixels = null;
+
+        private enum KinectFrameType
+        {
+            KinectColorFrame,
+            KinectDepthFrame
+        }
+
+        public ImageSource ColorImageSource
+        {
+            get
+            {
+                return this.colorBitmap;
+            }
+        }
+
+        public ImageSource DepthImageSource
+        {
+            get
+            {
+                return this.depthBitmap;
+            }
+        }
+
+
+        public MainWindow()
+        {
+            this.kinectSensor = KinectSensor.GetDefault();
+
+            this.colorFrameReader = kinectSensor.ColorFrameSource.OpenReader();
+            this.depthFrameReader = kinectSensor.DepthFrameSource.OpenReader();
+
+            this.colorFrameReader.FrameArrived += this.ColorFrameArrived;
+            this.depthFrameReader.FrameArrived += this.DepthFrameArrived;
+
+            // create image data
+            colorFrameDescription = kinectSensor.ColorFrameSource.CreateFrameDescription(ColorImageFormat.Bgra);
+            this.colorBitmap = new WriteableBitmap(colorFrameDescription.Width, colorFrameDescription.Height, 96, 96, PixelFormats.Bgr32, null);
+            depthFrameDescription = kinectSensor.DepthFrameSource.FrameDescription;
+            depthPixels = new byte[depthFrameDescription.Width * depthFrameDescription.Height];
+            this.depthBitmap = new WriteableBitmap(depthFrameDescription.Width, depthFrameDescription.Height, 96, 96, PixelFormats.Gray8, null);
+
+            kinectSensor.Open();
+
+            this.DataContext = this;
+
+            InitializeComponent();
+        }
+
+        private void ColorFrameArrived(object sender, ColorFrameArrivedEventArgs e)
+        {
+            using (ColorFrame colorFrame = e.FrameReference.AcquireFrame())
+            {
+                if (colorFrame != null)
+                {
+                    FrameDescription colorFrameDescription = colorFrame.FrameDescription;
+
+                    using (KinectBuffer colorBuffer = colorFrame.LockRawImageBuffer())
+                    {
+                        this.colorBitmap.Lock();
+
+                        // verify data and write the new color frame data to the display bitmap
+                        if ((colorFrameDescription.Width == this.colorBitmap.PixelWidth) && (colorFrameDescription.Height == this.colorBitmap.PixelHeight))
+                        {
+                            colorFrame.CopyConvertedFrameDataToIntPtr(
+                                this.colorBitmap.BackBuffer,
+                                (uint)(colorFrameDescription.Width * colorFrameDescription.Height * 4),
+                                ColorImageFormat.Bgra);
+
+                            this.colorBitmap.AddDirtyRect(new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight));
+                        }
+
+                        this.colorBitmap.Unlock();
+                    }
+                }
+            }
+        }
+
+        private void DepthFrameArrived(object sender, DepthFrameArrivedEventArgs e)
+        {
+            bool depthFrameProcessed = false;
+
+            // query depth frame
+            using (DepthFrame depthFrame = e.FrameReference.AcquireFrame())
+            {
+                if (depthFrame != null)
+                {
+                    // the fastest way to process the body index data is to directly access 
+                    // the underlying buffer
+                    using (Microsoft.Kinect.KinectBuffer depthBuffer = depthFrame.LockImageBuffer())
+                    {
+                        // verify data and write the color data to the display bitmap
+                        if (((this.depthFrameDescription.Width * this.depthFrameDescription.Height) == (depthBuffer.Size / this.depthFrameDescription.BytesPerPixel)) &&
+                            (this.depthFrameDescription.Width == this.depthBitmap.PixelWidth) && (this.depthFrameDescription.Height == this.depthBitmap.PixelHeight))
+                        {
+                            // Note: In order to see the full range of depth (including the less reliable far field depth)
+                            // we are setting maxDepth to the extreme potential depth threshold
+                            ushort maxDepth = ushort.MaxValue;
+
+                            // If you wish to filter by reliable depth distance, uncomment the following line:
+                            //// maxDepth = depthFrame.DepthMaxReliableDistance
+
+                            this.ProcessDepthFrameData(depthBuffer.UnderlyingBuffer, depthBuffer.Size, depthFrame.DepthMinReliableDistance, maxDepth);
+                            depthFrameProcessed = true;
+                        }
+                    }
+                }
+            }
+
+            if (depthFrameProcessed)
+            {
+                this.RenderDepthPixels();
+            }
+        }
+
+        /// <summary>
+        /// Directly accesses the underlying image buffer of the DepthFrame to 
+        /// create a displayable bitmap.
+        /// This function requires the /unsafe compiler option as we make use of direct
+        /// access to the native memory pointed to by the depthFrameData pointer.
+        /// </summary>
+        /// <param name="depthFrameData">Pointer to the DepthFrame image data</param>
+        /// <param name="depthFrameDataSize">Size of the DepthFrame image data</param>
+        /// <param name="minDepth">The minimum reliable depth value for the frame</param>
+        /// <param name="maxDepth">The maximum reliable depth value for the frame</param>
+        private unsafe void ProcessDepthFrameData(IntPtr depthFrameData, uint depthFrameDataSize, ushort minDepth, ushort maxDepth)
+        {
+            // depth frame data is a 16 bit value
+            ushort* frameData = (ushort*)depthFrameData;
+
+            // convert depth to a visual representation
+            for (int i = 0; i < (int)(depthFrameDataSize / this.depthFrameDescription.BytesPerPixel); ++i)
+            {
+                // Get the depth for this pixel
+                ushort depth = frameData[i];
+
+                // To convert to a byte, we're mapping the depth value to the byte range.
+                // Values outside the reliable depth range are mapped to 0 (black).
+                this.depthPixels[i] = (byte)(depth >= minDepth && depth <= maxDepth ? (depth / MapDepthToByte) : 0);
+            }
+        }
+
+        /// <summary>
+        /// Renders color pixels into the writeableBitmap.
+        /// </summary>
+        private void RenderDepthPixels()
+        {
+            this.depthBitmap.WritePixels(
+                new Int32Rect(0, 0, this.depthBitmap.PixelWidth, this.depthBitmap.PixelHeight),
+                this.depthPixels,
+                this.depthBitmap.PixelWidth,
+                0);
+        }
+
+        /// <summary>
+        /// release resources when closing the window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (this.colorFrameReader != null)
+            {
+                // ColorFrameReder is IDisposable
+                this.colorFrameReader.Dispose();
+                this.colorFrameReader = null;
+            }
+            if (this.depthFrameReader != null)
+            {
+                // ColorFrameReder is IDisposable
+                this.depthFrameReader.Dispose();
+                this.depthFrameReader = null;
+            }
+
+            if (this.kinectSensor != null)
+            {
+                this.kinectSensor.Close();
+                this.kinectSensor = null;
+            }
+        }
+
+        private void SaveFrame(string save_fn, KinectFrameType frame_type)
+        {
+            BitmapEncoder encoder = new PngBitmapEncoder();
+            switch (frame_type)
+            {
+                case KinectFrameType.KinectColorFrame:
+                    if(colorBitmap != null) 
+                        encoder.Frames.Add(BitmapFrame.Create(this.colorBitmap));
+                    break;
+                case KinectFrameType.KinectDepthFrame:
+                    if (depthBitmap != null) 
+                        encoder.Frames.Add(BitmapFrame.Create(this.depthBitmap));
+                    break;
+            }
+
+            if (encoder.Frames.Count == 0) 
+                return;
+            
+            // write the new file to disk
+            try
+            {
+                // FileStream is IDisposable
+                using (FileStream fs = new FileStream(save_fn, FileMode.Create))
+                {
+                    encoder.Save(fs);
+                }
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("fail to save color image");
+            }
+        }
+
+    }
+}
